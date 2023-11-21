704a0c0afd48a4c3d6e1972e9b6a26e2cb4975db
Jmint <jeffreymintah737@gmail.com>
2023-11-21 20:26:58 +0000

    my first commit

---
+++ DictApp/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from PyDictionary import PyDictionary
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def word(request):
+    search = request.GET.get('search')
+    dictionary = PyDictionary()
+    meaning = dictionary.meaning(search)
+    synonyms = dictionary.synonym(search)
+    antonyms = dictionary.antonym(search)
+
+    
+
+    formatted_meaning = format_meaning(meaning)
+
+    context = {
+        'word':search,
+        'formatted_meaning': formatted_meaning,
+        #'synonyms': synonyms,(Currently not available)
+        #'antonyms' : antonyms(Currently not available as well)
+    }
+    return render(request, 'word.html', context)
+
+def format_meaning(meaning):
+    if meaning:
+        formatted_meaning = {}
+        for part_of_speech, definitions in meaning.items():
+            formatted_definitions = "\n\n\n".join([f"-{definition}" for definition in definitions])
+            formatted_meaning[part_of_speech] = formatted_definitions
+        return formatted_meaning
+    else:
+        return None
+   
